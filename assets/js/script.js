085a9828422f8209101feb0dd8af106beb030b3c
chervepedia <168216306+chervepedia@users.noreply.github.com>
2024-07-21 18:15:41 +0300

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    // Настройки увеличения изображений с Medium Zoom
+    mediumZoom('.post-image-wrapper img', {
+        background: 'rgba(0, 0, 0, 0.7)'
+    });
+
+    // Плавная прокрутка наверх при клике на стрелку
+    $('.back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        return false;
+    });
+
+    // Показать/скрыть стрелку прокрутки наверх в зависимости от положения прокрутки
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }
+    });
+
+    // Открытие поста при клике на "читать далее"
+    $('.post-footer .read-more').click(function() {
+        var postUrl = $(this).data('url');
+        window.open(postUrl, '_blank');
+    });
+
+    // Показать/скрыть подменю при клике на пункт меню
+    $('.menu-link.has-submenu').click(function(e) {
+        e.preventDefault(); // Предотвращаем переход по ссылке
+
+        var $submenu = $(this).siblings('.sub-menu');
+
+        if ($submenu.is(':visible')) {
+            // Если подменю видно, скрываем его
+            $submenu.slideUp(); // Используем slideUp для скрытия
+        } else {
+            // Если подменю скрыто, показываем его
+            $submenu.slideDown(); // Используем slideDown для показа
+        }
+    });
+});
