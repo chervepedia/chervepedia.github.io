6296867a3e230b1849af65ddaf173a2a9b75114a
chervepedia <168216306+chervepedia@users.noreply.github.com>
2024-07-22 23:51:57 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function applyTwallpaper() {
-        if (window.matchMedia("(max-width: 767px)").matches) {
-            const options = {
-                fps: 60,
-                tails: 90,
-                animate: true,
-                scrollAnimate: true,
-                colors: [
-                    "#ffb0e2",
-                    "#ffb0e2",
-                    "#df36a1",
-                    "#560739"
-                ],
-                pattern: {
-                    image: "https://twallpaper.js.org/patterns/space.svg",
-                    background: "#000",
-                    blur: 0,
-                    size: "420px",
-                    opacity: 0.5,
-                    mask: true
-                }
-            };
-
-            // Инициализация twallpaper с указанными опциями
-            if (typeof twallpaper !== 'undefined') {
-                twallpaper.init(options);
-            } else {
-                console.error('twallpaper не загружен');
-            }
-        }
-    }
-
-    // Применяем фон сразу после загрузки
-    applyTwallpaper();
-
-    // Также добавляем обработчик событий на изменение размера окна
-    window.addEventListener('resize', applyTwallpaper);
-
-    // Плавная прокрутка наверх при клике на стрелку
+// Плавная прокрутка наверх при клике на стрелку
     document.querySelector('.back-to-top').addEventListener('click', function() {
         document.documentElement.scrollTop = 0;
         return false;
